a1c805930e8d0a36cd6f32e9e0bf8f92eeacd82b
Pau Fargas <pfargasreixats@gmail.com>
2025-09-25 13:48:45 +0200

    It works with parameters

--- JAX/mh_kernel.py
+++ JAX/mh_kernel.py
@@ -4,26 +4,26 @@ import jax.numpy as jnp
 from jax import random
 
 
-@partial(jax.jit, static_argnums=(1,))
-def mh_kernel(rng_key, prob_fn, position, prob):
+@partial(jax.jit, static_argnums=(1, 2))
+def mh_kernel(rng_key, prob_fn, prob_params, position, prob):
     key1, key2 = random.split(rng_key)
     proposal = position + random.normal(key1, shape=position.shape)
-    proposal_prob = prob_fn(proposal)
+    proposal_prob = prob_fn(proposal, prob_params)
     accept = jax.random.uniform(key2) < (proposal_prob / prob)
     new_position = jnp.where(accept, proposal, position)
     new_prob = jnp.where(accept, proposal_prob, prob)
     return new_position, new_prob
 
 
-@partial(jax.jit, static_argnums=(1, 2))
-def mh_chain(rng_key, n_steps, prob_fn, init_position):
+@partial(jax.jit, static_argnums=(1, 2, 3))
+def mh_chain(rng_key, n_steps, prob_fn, prob_params, init_position):
     def body_fn(i, val):
         key, position, prob = val
         _, key = random.split(key)
-        new_position, new_prob = mh_kernel(key, prob_fn, position, prob)
+        new_position, new_prob = mh_kernel(key, prob_fn, prob_params, position, prob)
         return key, new_position, new_prob
 
-    init_prob = prob_fn(init_position)
+    init_prob = prob_fn(init_position, prob_params)
     init_val = (rng_key, init_position, init_prob)
     _, positions, _ = jax.lax.fori_loop(0, n_steps, body_fn, init_val)
     return positions
@@ -31,31 +31,42 @@ def mh_chain(rng_key, n_steps, prob_fn, init_position):
 
 if __name__ == "__main__":
     rng_key = random.PRNGKey(0)
-    n_chains = 100000
+    n_chains = 10000
     n_steps = 100
+    s = 5.0
     init_position = jnp.zeros(n_chains)  # (nchains,)
     rng_keys = jax.random.split(rng_key, n_chains)  # (nchains,)
     print(rng_keys.shape)
-    run_mh_chain = jax.vmap(mh_chain, in_axes=(0, None, None, 0), out_axes=0)
-    samples = run_mh_chain(
-        rng_keys, n_steps, lambda x: jnp.exp(-0.5 * jnp.sum(x**2)), init_position
-    )
+    run_mh_chain = jax.vmap(mh_chain, in_axes=(0, None, None, None, 0), out_axes=0)
+
+    def prob_fn(x, s):
+        return jnp.exp(-0.5 * (x / s) ** 2) / (s * jnp.sqrt(2 * jnp.pi))
+
+    samples = run_mh_chain(rng_keys, n_steps, prob_fn, s, init_position)
     import matplotlib.pyplot as plt
     import numpy as np
 
-    samples.block_until_ready()
+    print("computing samples...")
+    samples.block_until_ready()  # stop the code until samples are ready
+    print("samples computed")
 
     # Convert JAX arrays to numpy before plotting
     samples_np = np.array(samples.flatten())
     print(samples_np.shape)
 
-    plt.hist(samples_np, bins=50, density=True, alpha=0.7, label="MCMC samples")
+    plt.hist(
+        samples_np,
+        bins=int(np.sqrt(n_chains)),
+        density=True,
+        alpha=0.7,
+        label="MCMC samples",
+    )
 
-    x = jnp.linspace(-4, 4, 100)
+    x = jnp.linspace(min(samples_np), max(samples_np), 500)
     x_np = np.array(x)
-    y_np = np.array(jnp.exp(-0.5 * x**2) / jnp.sqrt(2 * jnp.pi))
+    y_np = np.array(prob_fn(x, s))
 
-    plt.plot(x_np, y_np, "r-", linewidth=2, label="True distribution")
+    plt.plot(x_np, y_np, "r-", linewidth=2, label="True distribution", alpha=0.7)
     plt.legend()
     plt.xlabel("Value")
     plt.ylabel("Density")
