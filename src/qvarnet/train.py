c5fea75afb6dcaa7315f1ee54c317a0bcc18e215
Pau Fargas <pfargasreixats@gmail.com>
2026-03-16 13:21:12 +0100

    bugfix

--- src/qvarnet/train.py
+++ src/qvarnet/train.py
@@ -295,7 +295,7 @@ def train(
         # Normalize state parameters to prevent numerical issues (optional, can be tuned or removed)
         wf_norm_squared = jnp.sum(jnp.square(state.apply_fn(state.params, batch)))
         norm_factor = jnp.sqrt(wf_norm_squared + 1e-8)
-        new_params = jax.tree_map(lambda p: p / norm_factor, new_state.params)
+        new_params = jax.tree.map(lambda p: p / norm_factor, new_state.params)
         new_state = new_state.replace(params=new_params)
 
         return new_state, key, current_pos, E, sigma_e, acceptance_rate, step_size
