fix: Update acceptance condition in mh_kernel_log to use log probabilities for numerical stability

src/qvarnet/hamiltonian/kinetic.py
@@ -79,10 +79,11 @@ def kinetic_term_log_wavefunction(params, samples, model_apply, laplacian=laplac
     # 2. Compute Laplacian of Log Psi (Trace of Hessian)
     # Forward-mode AD is faster and safer for Laplacians
 
-    lap_val = jax.vmap(laplacian)(samples)
+    # lap_val = jax.vmap(laplacian)(samples)
+    lap_psi = laplacian(params, samples, model_apply)
 
     # 3. Kinetic Energy Formula (Log Domain)
     # T = -0.5 * ( Laplacian(ln Psi) + |Grad(ln Psi)|^2 )
-    kinetic = -0.5 * (lap_val + jnp.sum(grad_val**2, axis=-1))
+    kinetic = -0.5 * (lap_psi + jnp.sum(grad_val**2, axis=-1))
 
     return kinetic

src/qvarnet/samplers/sampler.py
@@ -28,7 +28,7 @@ def mh_kernel_log(
     accept_log_prob = jnp.minimum(
         0.0, proposal_log_prob - prob
     )  # log(accept_prob) = min(0, log(proposal_prob) - log(current_prob))
-    accept = uniform_random_numbers[-1] < jnp.exp(accept_log_prob)
+    accept = jnp.log(uniform_random_numbers[-1]) < accept_log_prob
     new_position = jnp.where(accept, proposal, position)
     new_log_prob = jnp.where(accept, proposal_log_prob, prob)
     return new_position, new_log_prob, accept